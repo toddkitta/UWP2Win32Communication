17bf43bed6ec58acbbf521c779bcc468b9b3a8a3
toddkitta <toddkitta@outlook.com>
2016-03-04 22:54:57 -0600

    Initial commit

---
+++ UWPSender/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Web.Http;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPSender
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Do();
+        }
+
+        private async void Do()
+        {
+            HttpClient http = new HttpClient();
+            var headers = http.DefaultRequestHeaders;
+            headers.Add("MyCustomHeader", textBox.Text);
+
+            Uri requestUri = new Uri("http://localhost:54321/");
+
+            try
+            {
+                HttpResponseMessage httpResponse = await http.GetAsync(requestUri);
+                httpResponse.EnsureSuccessStatusCode();
+                string httpResponseBody = await httpResponse.Content.ReadAsStringAsync();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+    }
+}
---
+++ Win32Listener/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Win32Listener
+{
+    public class Program
+    {
+        private static readonly string[] prefixes = null;
+
+        static Program()
+        {
+            prefixes = new string[] { "http://localhost:54321/" };
+        }
+
+        public static void Main(string[] args)
+        {
+            if (!HttpListener.IsSupported)
+            {
+                Console.WriteLine("Nope...");
+                return;
+            }
+
+            HttpListener listener = new HttpListener();
+            foreach (string s in prefixes)
+            {
+                listener.Prefixes.Add(s);
+            }
+
+            listener.Start();
+
+            Console.CancelKeyPress += delegate
+            {
+                listener.Stop();
+            };
+
+            while (true)
+            {
+                Console.WriteLine("Listening...");
+
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerRequest request = context.Request;
+                HttpListenerResponse response = context.Response;
+
+                string responseMessage = $"I heard you... you said '{request.Headers["MyCustomHeader"]}'";
+
+                byte[] buffer = Encoding.UTF8.GetBytes(responseMessage);
+                Console.WriteLine(responseMessage);
+
+                response.ContentLength64 = buffer.Length;
+                using (Stream output = response.OutputStream)
+                {
+                    output.Write(buffer, 0, buffer.Length);
+                    output.Close();
+                }
+            }
+        }
+    }
+}
